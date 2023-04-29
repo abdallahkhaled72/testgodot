65a7117a0d7cf7a0f3a48676b9c0c17e5fde21bb
Abdullah Khaled <abdullakhalid7857166@gmail.com>
2023-04-30 01:25:50 +0300

    done

---
+++ FloorCelling.gd
@@ -0,0 +1,6 @@
+extends Area2D
+
+export var _bounce_direction = 1
+func _on_area_extered(area):
+	if area.name == "Ball":
+		area.direction = (area.direction + Vector2(0, _bounce_direction)).normalized()
